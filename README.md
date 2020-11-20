a09f51d9923c9ac7e4a756d348a1c9c748fd421c
Michael Kellman <kellmanmr@gmail.com>
2020-11-20 15:40:31 -0800

    first push

---
+++ README.md
@@ -0,0 +1,1 @@
+A complex number module for Pytorch.
---
+++ compyt/__init__.py
@@ -0,0 +1,22 @@
+# import compyt
+
+from compyt import ComplexMul
+from compyt import ComplexDiv
+from compyt import ComplexAbs
+from compyt import ComplexAbs2
+from compyt import ComplexExp
+
+from compyt import rn2cp, cp2cn, cp2rn, cp2in, cn2cp
+
+from compyt import angle, amp, conj
+
+from compyt import mul_c, div_c, abs_c, abs2_c, exp_c
+
+# from compyt import *
+# import compyt
+
+# mul_c  = ComplexMul().apply
+# div_c  = ComplexDiv().apply
+# abs_c  = ComplexAbs().apply
+# abs2_c = ComplexAbs2().apply 
+# exp_c  = ComplexExp().apply
